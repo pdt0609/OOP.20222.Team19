1bbc5f7de21649b16c8bea3e9dd72fd2d53a4e14
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-07-12 17:52:07 +0700

    rename src to sourcecode

--- O-AN-QUAN/sourcecode/model/board/Square.java
+++ O-AN-QUAN/sourcecode/model/board/Square.java
@@ -1,8 +1,11 @@
 package model.board;
-public class Square extends Cell  {
+public class Square extends Cell implements Pickable {
     public Square(int location, int numberOfGems) {
         super(location, numberOfGems);
     }
 
-
+	@Override
+	public boolean isPickable() {
+		return true;
+	}
 }
