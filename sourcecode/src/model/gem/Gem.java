9672be03b03aedad8cee4eb5c51e430a1bf92ecc
Đoàn Trọng Tân <103823936+ttan142@users.noreply.github.com>
2023-07-12 22:59:28 +0700

    Update Gem.java

--- sourcecode/src/model/gem/Gem.java
+++ sourcecode/src/model/gem/Gem.java
@@ -3,19 +3,3 @@ package model.gem;
 public abstract class Gem {
     public abstract int getValue();
 }
-
-public class BigGem extends Gem {
-    private final int value = 5;
-
-    public int getValue() {
-        return value;
-    }
-}
-
-public class SmallGem extends Gem {
-    private final int value = 1;
-
-    public int getValue() {
-        return value;
-    }
-}
