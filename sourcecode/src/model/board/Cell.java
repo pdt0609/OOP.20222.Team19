08a983bd02fc8f5bcebfb13cb1f78a1cdcf8603e
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-07-12 18:13:47 +0700

    build basic class cell

---
+++ sourcecode/src/model/board/Cell.java
@@ -0,0 +1,39 @@
+package model.board;
+
+import java.util.ArrayList;
+
+
+public abstract class Cell {
+    private int location;
+    private int numberOfGems;
+
+    public Cell(int location, int numberOfGems) {
+        this.location = location;
+        this.numberOfGems = numberOfGems;
+    }
+
+    public Cell(int location) {
+        this.location = location;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public int getNumberOfGems() {
+        return numberOfGems;
+    }
+
+    public void addGem(Gem gem) {
+        this.gemList.add(gem);
+    }
+
+    public boolean isEmpty() {
+        return this.getGemList().size() == 0;
+    }
+
+    public void setEmpty(){
+        this.gemList.clear();
+    }
+
+}
