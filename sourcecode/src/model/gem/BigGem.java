2fb61b401a15837b015ad9e1facb489bbba4bc81
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-07-13 15:49:58 +0700

    update gem

--- sourcecode/src/model/gem/BigGem.java
+++ sourcecode/src/model/gem/BigGem.java
@@ -3,6 +3,7 @@ package model.gem;
 public class BigGem extends Gem {
     private final int value = 5;
 
+    @Override
     public int getValue() {
         return value;
     }
--- sourcecode/src/model/gem/SmallGem.java
+++ sourcecode/src/model/gem/SmallGem.java
@@ -3,6 +3,7 @@ package model.gem;
 public class SmallGem extends Gem {
     private final int value = 1;
 
+    @Override
     public int getValue() {
         return value;
     }
